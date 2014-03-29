31a38a6a337d2c1312ee427f0ffd2019ec15da19
610152753 <qiaoshunlong@163.com>
2014-03-29 12:48:07 +0800

    add

--- test/src/properties/ConfigGetDb.java
+++ test/src/properties/ConfigGetDb.java
@@ -6,7 +6,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
-
+ 
 public class ConfigGetDb
 {
 	public String url = null;
