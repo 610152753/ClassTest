3e7e7c4a0f2e6c261b59acc59c99c381aaf66823
610152753 <qiaoshunlong@163.com>
2014-04-05 22:34:47 +0800

    add Reflection
    Change-Id: I1da291e455bafdf00d6169f8183d605cdacaf566
    Signed-off-by: 610152753 <qiaoshunlong@163.com>

---
+++ test/src/reflection/DumpMethods.java
@@ -0,0 +1,21 @@
+package reflection;
+
+import java.lang.reflect.Method;
+
+public class DumpMethods
+{
+	// 从命令行接受一个字符串(该字符串是某个类的全名)
+	// 打印出该类中的所有方法申明
+	public static void main(String[] args) throws ClassNotFoundException
+	{
+		//Class 类是java反射的入口点
+		Class classType = Class.forName(args[0]); 
+		
+			Method[] methods = classType.getDeclaredMethods();
+			
+			for(Method method : methods){
+				
+				System.out.println(method);
+			}
+	}
+}
