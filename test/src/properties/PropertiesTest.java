3db4b59ee3e1a488b3caab519580792f34692eef
610152753 <qiaoshunlong@163.com>
2014-06-22 21:38:29 +0800

    add VectorTest.java
    Change-Id: I7b928b18ef7f394775677e623645254d8b94eb66
    Signed-off-by: 610152753 <qiaoshunlong@163.com>

---
+++ test/src/properties/PropertiesTest.java
@@ -0,0 +1,29 @@
+package properties;
+
+import java.util.Iterator;
+import java.util.Properties;
+import java.util.Set;
+
+/**
+ * 获取系统环境变量
+ * 
+ * @author JoePotter
+ *
+ */
+public class PropertiesTest
+{
+	public static void main(String[] args)
+	{
+		Properties p = System.getProperties();
+		
+		Set set = p.keySet();
+		
+		for(Iterator iter = set.iterator();iter.hasNext();)
+		{
+		   String key = (String)iter.next();
+		   String value = p.getProperty(key);
+		   
+		   System.out.println(key + "=" + value);
+		}
+	}
+}
--- test/src/strategyTestPerson/Client.java
+++ test/src/strategyTestPerson/Client.java
@@ -13,11 +13,11 @@ public class Client
 		personList.add(new Person(9, "zhangsan", 20));
 		personList.add(new Person(2, "lisi", 21));
         personList.add(new Person(3, "wangwu", 25));
-        personList.add(new Person(4, "zhaoliu", 26));
+        personList.add(new Person(8, "zhaoliu", 26));
         personList.add(new Person(5, "chenqi", 28));
         personList.add(new Person(6, "dongba", 10));
         personList.add(new Person(7, "zhangsan", 15));
-        personList.add(new Person(8, "liujiu", 26));
+        personList.add(new Person(4, "liujiu", 26));
  
         /*
          * 可通过这里new不同的Comparator的实现从而实现不同的排序策略
@@ -29,9 +29,9 @@ public class Client
          * AgeOrder：按年龄正序
          * AgeReverseOrder：按年龄逆序
          */
-        Comparator<Person> comp = new NameOrder();
+        Comparator<Person> comp = new AgeOrder();
         Collections.sort(personList, comp);
-        System.out.println(((NameOrder)comp).getMethodName()+"  排序后的序列是:");
+        System.out.println(((AgeOrder)comp).getMethodName()+"  排序后的序列是:");
         System.out.println("-----------------------");
         for (Iterator<Person> itor=personList.iterator();itor.hasNext();) {
             Person p=itor.next();
