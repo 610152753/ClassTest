36c453bf332bc35bbc36acb81946f469bdab4a5a
610152753 <qiaoshunlong@163.com>
2014-09-24 21:09:55 +0800

    ByteArrayInputStreamTest add

---
+++ test/src/ioTest/BufferedOutputStreamTest.java
@@ -0,0 +1,25 @@
+package ioTest;
+
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+
+public class BufferedOutputStreamTest {
+	
+	public static void main(String[] args) throws Exception {
+		
+		OutputStream os = new FileOutputStream("1.txt");
+		
+		
+		//»º³åÊä³öÁ÷
+		BufferedOutputStream bos = new BufferedOutputStream(os);
+		
+		bos.write("http://www.google.com".getBytes());
+		
+		bos.close();
+//		
+//		os.close();
+		
+	}
+
+}
