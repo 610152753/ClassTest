642ba75a570c7d1b6a7ed00347efb8cf721dbd89
610152753 <qiaoshunlong@163.com>
2015-05-04 21:40:05 +0800

    Socket Test

---
+++ test/src/socket/TcpSocketClient.java
@@ -0,0 +1,34 @@
+package socket;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class TcpSocketClient
+{
+	
+	public static void main(String[] args) throws Exception {
+		
+		Socket socket = new Socket("127.0.0.1",5000);
+		
+		InputStream is = socket.getInputStream();
+		
+		OutputStream os = socket.getOutputStream();
+		
+		os.write("Hello World !".getBytes());
+		
+		byte[] buffer = new byte[200];
+		
+		int length = is.read(buffer);
+		
+		System.out.println(new String(buffer,0,length));
+		
+		is.close();
+		
+		os.close();
+		
+		socket.close();
+		
+	}
+
+}
---
+++ test/src/socket/TcpSocketServer.java
@@ -0,0 +1,39 @@
+package socket;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+public class TcpSocketServer {
+	
+	public static void main(String[] args) throws Exception {
+		
+		ServerSocket ss = new ServerSocket(5000);
+		
+		Socket socket = ss.accept();
+		
+		InputStream is = socket.getInputStream();
+		
+		OutputStream os = socket.getOutputStream();
+		
+		byte[] buffer = new byte[200];
+		
+		int length = is.read(buffer);
+		
+		System.out.println(new String(buffer,0,length));
+		
+		os.write("Welcome !".getBytes());
+		
+		is.close();
+		
+		os.close();
+		
+		socket.close();
+		
+		ss.close();
+		
+	}
+
+}
